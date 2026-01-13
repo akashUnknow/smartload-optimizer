e54094a8cbec58263cca685ad83b3f377b0934b1
akashUnknow <akash.unknow@gmail.com>
2026-01-13 22:22:29 +0530

    isuue has been solved need to work on service layer

--- src/main/java/com/akash/optimizer/service/LoadOptimizerService.java
+++ src/main/java/com/akash/optimizer/service/LoadOptimizerService.java
@@ -2,7 +2,9 @@ package com.akash.optimizer.service;
 
 import com.akash.optimizer.model.OptimizeRequest;
 import com.akash.optimizer.model.OptimizeResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoadOptimizerService {
     public OptimizeResponse optimize(OptimizeRequest request) {
         return null;
